09de72e652656bb239b547dd37639a94940b4c49
Zaki Jamali Arafi <zakijamaliarafi@gmail.com>
2023-11-26 13:19:02 +0700

    update query insert

--- signup_process.php
+++ signup_process.php
@@ -12,8 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Username sudah ada. Silakan coba lagi.";
     } else {
         // Tambahkan user ke database
-        $hash_password = password_hash($password, PASSWORD_DEFAULT);
-        $insert_query = "INSERT INTO user (username, password, role_user) VALUES ('$username', '$hash_password', 'pinocchio')";
+        $insert_query = "INSERT INTO user (username, password, role_user) VALUES ('$username', '$password', 'pinocchio')";
         if ($conn->query($insert_query) === TRUE) {
             header("Location: login.php");
         } else {
