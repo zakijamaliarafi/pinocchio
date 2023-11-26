704960a10ec8574ab0475d741004c0e2a304442a
Zaki Jamali Arafi <zakijamaliarafi@gmail.com>
2023-11-26 09:34:52 +0700

    menambahkan proses signup

--- signup_process.php
+++ signup_process.php
@@ -1,11 +1,25 @@
 <?php
+include 'koneksi.php';
 
-$username = $_POST['username'];
-$password = $_POST['password'];
-$confirm = $_POST['confirm'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    // Periksa apakah username sudah ada
+    $check_query = "SELECT * FROM user WHERE username = '$username'";
+    $result = $conn->query($check_query);
 
-echo $username;
-echo $password;
-echo $confirm;
-
+    if ($result->num_rows > 0) {
+        echo "Username sudah ada. Silakan coba lagi.";
+    } else {
+        // Tambahkan user ke database
+        $hash_password = password_hash($password, PASSWORD_DEFAULT);
+        $insert_query = "INSERT INTO user (username, password, role_user) VALUES ('$username', '$hash_password', 'pinocchio')";
+        if ($conn->query($insert_query) === TRUE) {
+            header("Location: login.html");
+        } else {
+            echo "Error: " . $insert_query . "<br>" . $conn->error;
+        }
+    }
+}
+$conn->close();
 ?>
